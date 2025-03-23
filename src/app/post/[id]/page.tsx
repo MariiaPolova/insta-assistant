6779f6f23431aa39619c20e03068101e890f95ee
Mariia Polova <maria.polevaja@gmail.com>
2025-03-23 12:33:55 +0200

    feat: single post view, image from canvas average color

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,15 +1,18 @@
 'use client'
-import { Fragment } from "react";
+import { Fragment, useState, useMemo } from "react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import usePost from "../../../lib/hooks/usePost";
 import LoadingSpinner from "../../../components/common/Spinner";
 import ErrorMessage from "../../../components/common/ErrorMessage";
+import { getAverageRGB } from "../../../lib/helpers/pickImageColor";
 
 export default function PostInfoPage() {
   const { id } = useParams() as { id: string };
 
   const { post, error: postError, isLoading: postIsLoading } = usePost(id);
+  const [image, setImage] = useState(null);
+  const color = useMemo(() => getAverageRGB(image), [image]);
 
   if (!post && postIsLoading) {
     return <LoadingSpinner />;
@@ -22,8 +25,9 @@ export default function PostInfoPage() {
 
   return (
     <Fragment>
-      <article className="m-3 py-10 px-4 sm:px-6 lg:px-8 flex gap-y-4 lg:flex-row flex-col bg-gray-100 rounded-lg">
+      <article className="m-3 py-10 px-4 sm:px-6 lg:px-8 flex gap-y-4 lg:flex-row flex-col bg-gray-100 rounded-lg min-h-[95vh]">
         <Image
+          onLoad={(e) => setImage(e.target)}
           className="w-35 object-cover max-sm:h-80 sm:aspect-[2/1] lg:aspect-square rounded-lg"
           src={post.display_url}
           alt="food image"
@@ -37,12 +41,33 @@ export default function PostInfoPage() {
             </time>
             <a
               href={post.url}
-              className="relative z-10 rounded-full bg-gray-200 px-3 py-1.5 font-medium text-gray-600 hover:bg-blue-100"
+              className="relative z-10 rounded-full px-3 py-1.5 font-medium text-white hover:bg-blue-100"
+              style={{
+                backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, .7)`,
+                borderColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
+                boxShadow: `0 0 0 1px rgba(${color.r}, ${color.g}, ${color.b}, 0.05)`
+              }}
             >
               Link to {post.account_username}&apos;s post
             </a>
           </div>
           <p className="mt-5 whitespace-break-spaces text-sm/6 text-gray-600">{post.caption}</p>
+          <div className="mt-6">
+            {post.hashtags.map((hashtag, i) =>
+              <span
+                key={`${post.id}-${i}`}
+                className="inline-flex items-center rounded-md mx-1 px-2 my-1 py-2 text-xs font-lg ring-1 ring-inset"
+                style={{
+                  backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, .1)`,
+                  color: `rgb(${color.r}, ${color.g}, ${color.b})`,
+                  borderColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
+                  boxShadow: `0 0 0 1px rgba(${color.r}, ${color.g}, ${color.b}, 0.05)`
+                }}
+              >
+                {hashtag}
+              </span>
+            )}
+          </div>
         </div>
       </article>
     </Fragment>
---
+++ src/lib/helpers/pickImageColor.ts
@@ -0,0 +1,43 @@
+export function getAverageRGB(imgEl) {
+    const blockSize = 5; // only visit every 5 pixels
+    const defaultRGB = { r: 0, g: 0, b: 0 }; // for non-supporting envs
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext && canvas.getContext('2d');
+    const rgb = { r: 0, g: 0, b: 0 };
+    let data,
+        i = -4,
+        count = 0;
+
+    if (!context || !imgEl) {
+        return defaultRGB;
+    }
+
+    const height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    const width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+    context.drawImage(imgEl, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch (e) {
+        console.log(e);
+        return defaultRGB;
+    }
+
+    const length = data.data.length;
+
+    while ((i += blockSize * 4) < length) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i + 1];
+        rgb.b += data.data[i + 2];
+    }
+
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r / count);
+    rgb.g = ~~(rgb.g / count);
+    rgb.b = ~~(rgb.b / count);
+
+    return rgb;
+
+}
