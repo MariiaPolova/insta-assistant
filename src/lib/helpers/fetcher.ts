e661bccb934d1f362b47eca4675109b09c87ee71
Mariia Polova <maria.polevaja@gmail.com>
2025-09-06 10:27:13 +0300

    chore: add docker file

--- src/lib/helpers/fetcher.ts
+++ src/lib/helpers/fetcher.ts
@@ -1,20 +1,20 @@
-const updateOptions = () => {
-  if (typeof window === "undefined") return {};
+// const updateOptions = () => {
+//   if (typeof window === "undefined") return {};
 
-  if (!window.localStorage.user) return {};
+//   if (!window.localStorage.user) return {};
 
-  if (Object.keys(window.localStorage.user).length === 0) return {};
+//   if (Object.keys(window.localStorage.user).length === 0) return {};
 
-  const user = JSON.parse(window.localStorage.user);
+//   const user = JSON.parse(window.localStorage.user);
 
-  if (!!user.token) {
-    return {
-      headers: {
-        Authorization: `Token ${user.token}`,
-      },
-    };
-  }
-};
+//   if (!!user.token) {
+//     return {
+//       headers: {
+//         Authorization: `Token ${user.token}`,
+//       },
+//     };
+//   }
+// };
 
 async function fetcher (url) {
   try {
