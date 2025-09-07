fcc2219094a2650f263f20b507dbe2401401720d
Mariia Polova <maria.polevaja@gmail.com>
2025-09-07 23:14:24 +0300

    fix: api url env

--- src/lib/hooks/useList.ts
+++ src/lib/hooks/useList.ts
@@ -2,7 +2,7 @@ import useSWR from "swr"
 import fetcher from "../helpers/fetcher";
 
 function useList<T>(id?: string) {
-    const { data, error, isLoading, mutate } = useSWR<T>(id ? `${process.env.NEXT_PUBLIC_API_URL}/api/lists/${id}` : `${API_URL}/api/lists`,
+    const { data, error, isLoading, mutate } = useSWR<T>(id ? `${process.env.NEXT_PUBLIC_API_URL}/api/lists/${id}` : `${process.env.NEXT_PUBLIC_API_URL}/api/lists`,
         fetcher,
         { revalidateOnFocus: false }
       );
